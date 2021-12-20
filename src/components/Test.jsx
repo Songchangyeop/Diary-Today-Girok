98c7be2721727d8035f7338978a388644f90c9c7
Songchangyeop <waitwait0301@naver.com>
2021-12-20 15:18:18 +0900

    FIX: Netlify Page Not Found 오류 해결

--- src/components/Test.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Test = () => {
-	return <div>안녕나야</div>;
-};
-
-export default Test;
--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -4,61 +4,62 @@ import CardEditForm from '../card_edit_form/card_edit_form';
 import styles from './editor.module.css';
 
 const Editor = ({
-  FileInput,
-  cards,
-  createCard,
-  updateCard,
-  deleteCard,
-  editorOpen,
-  editOpen,
-  month,
-  showFeelComponent,
-  currentEmotion,
-  changeClickedEditor,
+	FileInput,
+	cards,
+	createCard,
+	updateCard,
+	deleteCard,
+	editorOpen,
+	editOpen,
+	month,
+	showFeelComponent,
+	currentEmotion,
+	changeClickedEditor,
 }) => {
-  const btnClick = (event) => {
-    editorOpen(event.target.value);
-  };
-  return (
-    <section className={styles.editor}>
-      {editOpen && (
-        <button
-          className={styles.close}
-          type="button"
-          onClick={btnClick}
-          value="close"
-        >
-          닫기
-        </button>
-      )}
+	const btnClick = (event) => {
+		editorOpen(event.target.value);
+	};
 
-      <h1 className={styles.title}>일기 쓰기</h1>
-      <CardAddForm
-        FileInput={FileInput}
-        onAdd={createCard}
-        month={month}
-        cards={cards}
-        showFeelComponent={showFeelComponent}
-        currentEmotion={currentEmotion}
-      />
-      <div className={styles.bar}></div>
-      <h1 className={styles.title}>일기 편집</h1>
-      {Object.keys(cards).map((key) => (
-        <CardEditForm
-          key={key}
-          FileInput={FileInput}
-          card={cards[key]}
-          updateDay={updateCard}
-          updateCard={createCard}
-          deleteCard={deleteCard}
-          month={month}
-          cards={cards}
-          showFeelComponent={showFeelComponent}
-          changeClickedEditor={changeClickedEditor}
-        />
-      ))}
-    </section>
-  );
+	return (
+		<section className={styles.editor}>
+			{editOpen && (
+				<button
+					className={styles.close}
+					type="button"
+					onClick={btnClick}
+					value="close"
+				>
+					닫기
+				</button>
+			)}
+
+			<h1 className={styles.title}>일기 쓰기</h1>
+			<CardAddForm
+				FileInput={FileInput}
+				onAdd={createCard}
+				month={month}
+				cards={cards}
+				showFeelComponent={showFeelComponent}
+				currentEmotion={currentEmotion}
+			/>
+			<div className={styles.bar}></div>
+			<h1 className={styles.title}>일기 편집</h1>
+			{Object.keys(cards).map((key) => (
+				<CardEditForm
+					key={key}
+					FileInput={FileInput}
+					card={cards[key]}
+					updateDay={updateCard}
+					updateCard={createCard}
+					deleteCard={deleteCard}
+					month={month}
+					cards={cards}
+					showFeelComponent={showFeelComponent}
+					changeClickedEditor={changeClickedEditor}
+				/>
+			))}
+		</section>
+	);
 };
 
 export default Editor;
